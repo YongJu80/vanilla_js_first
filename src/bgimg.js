e38ca260c61cbadb8b17a86e7e38dee75ad540d9
YongJu <61091345+YongJu80@users.noreply.github.com>
2022-01-22 23:00:48 +0900

    v0.2

---
+++ src/bgimg.js
@@ -0,0 +1,14 @@
+const bg = document.querySelector("body");
+
+let bg_img = [];
+
+const RandomImg = () => {
+  fetch(`https://source.unsplash.com/featured/?maldives-beach/1920x1080`).then(
+    (response) => {
+      document.body.style.backgroundImage = `url(${response.url})`;
+      document.body.style.backgroundSize = "cover";
+    }
+  );
+};
+
+window.addEventListener("load", RandomImg);
