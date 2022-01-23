82213d1601a906da808fcbce138d30169bf9b804
YongJu <61091345+YongJu80@users.noreply.github.com>
2022-01-23 09:50:19 +0900

    v0.21

--- src/bgimg.js
+++ src/bgimg.js
@@ -1,7 +1,5 @@
 const bg = document.querySelector("body");
 
-let bg_img = [];
-
 const RandomImg = () => {
   fetch(`https://source.unsplash.com/featured/?landscape-hd/1920x1080`).then(
     (response) => {
